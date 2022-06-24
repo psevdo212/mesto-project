ca4a3f5bcf612d5783a99a6d0aae3e3a546896bb
psevdo <psevdo@MacBook-Pro-Mac.local>
2022-06-24 18:50:33 +0300

    1234

--- script/index.js
+++ script/index.js
@@ -1,8 +1,8 @@
 "use strict";
 // редактирование профиля
 const editButton = document.querySelector(".profile__edit-button");
-const closePopupButton = document.querySelector(".popup__close-button");
-const popup = document.querySelector(".popup");
+const closeEditButton = document.querySelector(".popup__close-button");
+const editPopup = document.querySelector(".popup");
 const addButton = document.querySelector(".profile__add-button");
 const closeNewplaceButton = document.querySelector(".newplace__close-button");
 const imgCloseButton = document.querySelector(".image-big__close-button");
@@ -13,68 +13,36 @@ const profileTitle = document.querySelector(".profile__title");
 const profileSubtitle = document.querySelector(".profile__subtitle");
 const imgBig = document.querySelector(".image-big");
 const closeBigImg = document.querySelector(".image-big__close-button");
+const editForm = document.querySelector(".popup__form");//поиск формы редактирования профиля
+const newplaceFormElement = document.querySelector(".newplace__form");//поиск формы добавления места
 
-function popupToggle() {
-  popup.classList.toggle("popup_opened"); // если класса нет - добавляется, если есть - убираем
+//МОДАЛЬНЫЕ ОКНА
+function openModalWindow (modalWindow) {//открытие модалки
+  modalWindow.classList.add('opened');
+}
+
+function closeModalWindow (modalWindow) {//закрытие модалки
+  modalWindow.classList.remove('opened');
+}
+
+//РЕДАКТИРОВАНИЕ ПРОФИЛЯ
+function openEditPopup() {//открытие модалки редактирования
   username.value = profileTitle.textContent; // Заменяем заглушку на имя из профиля
   description.value = profileSubtitle.textContent;
+  openModalWindow(editPopup);
 }
-editButton.addEventListener("click", popupToggle);
-closePopupButton.addEventListener("click", popupToggle);
 
-//добавление места
-
-function newplaceToggle() {
-  newplace.classList.toggle("newplace_opened"); // если класса нет - добавляется, если есть - убираем
-}
-addButton.addEventListener("click", newplaceToggle);
-closeNewplaceButton.addEventListener("click", newplaceToggle);
-
-// отправка формы редактирования профиля
-// Находим форму в DOM
-const formElement = document.querySelector(".popup__form");
-
-// Обработчик «отправки» формы
-function formSubmitHandler(evt) {
+// Отправка формы редактирования профиля
+function editFormSubmitHandler(evt) {
   evt.preventDefault();
   const name = username.value;
   const job = description.value;
   profileTitle.textContent = name; //заменяю текс профиля значением из поля формы
   profileSubtitle.textContent = job;
-  popupToggle(); //закрываем окно после нажатия кнокпи Сохранить
+  closeModalWindow(editPopup); //закрываем окно после нажатия кнопки Сохранить
 }
 
-// Прикрепляем обработчик к форме:
-formElement.addEventListener("submit", formSubmitHandler);
-
-//ДОБАВЛЕНИЕ КАРТОЧЕК
-const initialCards = [
-  {
-    name: "Архыз",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-  },
-  {
-    name: "Челябинская область",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-  },
-  {
-    name: "Иваново",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-  },
-  {
-    name: "Камчатка",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-  },
-  {
-    name: "Холмогорский район",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-  },
-  {
-    name: "Байкал",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-  },
-];
-
+//МЕСТА
 //добавляем первые шесть карточек
 function addDefaultCards() {
   for (let card of initialCards) {
@@ -83,7 +51,20 @@ function addDefaultCards() {
 }
 addDefaultCards();
 
+//добавление места через кнопку
+function openNewplace() {
+  openModalWindow(newplace);
+}
+
 // добавление по одной
+function createCard(name, link) {
+  //создается DOM элемент карточки
+  //в карточку вставляются данные и навешиваются обработчики
+  return element; //возвращается созданная карточка
+}
+
+
+
 function addCard(placeName, placeUrl) {
   const placeContainer = document.querySelector(".places"); //тут размещаются карточки
   const placeTemplate = document.querySelector("#place-template").content; //получаю содержимое шаблона
@@ -104,7 +85,7 @@ function addCard(placeName, placeUrl) {
       document.querySelector(".image-big__image").src = placeUrl;
       document.querySelector(".image-big__image").alt = placeName;
       document.querySelector(".image-big__figcaption").textContent = placeName;
-      imgBigToggle();
+      openImgBig();
     });
   placeElement
     .querySelector(".place__delete")
@@ -115,24 +96,39 @@ function addCard(placeName, placeUrl) {
     });
 }
 
-// отправка формы нового места
-const newplaceFormElement = document.querySelector(".newplace__form");
 
-// Обработчик «отправки» формы
+
+// Отправка формы нового места
 function newplaceSubmitHandler(evt) {
   evt.preventDefault();
   const name = document.querySelector("#place-name"); //выбор поля с названием
   const url = document.querySelector("#image-link"); //поле со ссылкой
   addCard(name.value, url.value); //передаем содержимое полей в функцию добавления
-  newplaceToggle(); //закрываем окно после нажатия кнопки Сохранить
+  closeModalWindow(newplace); //закрываем окно после нажатия кнопки Сохранить
 }
 
-// Прикрепляем обработчик к форме:
+
+//Открытие модалки с большой картинкой
+function openImgBig() {
+  openModalWindow(imgBig);
+}
+
+//СОБЫТИЯ
+closeBigImg.addEventListener("click", function() {
+  closeModalWindow(imgBig);
+});//открытие большой картинки
+editButton.addEventListener("click", openEditPopup);//открытие окна редактирования профиля
+closeEditButton.addEventListener("click", function() {//закрытие модалки редактирования
+  closeModalWindow(editPopup);
+});
+addButton.addEventListener("click", openNewplace);//открытие места
+closeNewplaceButton.addEventListener("click", function() {//закрытие места
+  closeModalWindow(newplace);
+});
+
+// Прикрепляем обработчик к форме редактирования профиля:
+editForm.addEventListener("submit", editFormSubmitHandler);
+
+// Прикрепляем обработчик к форме нового места:
 newplaceFormElement.addEventListener("submit", newplaceSubmitHandler);
 
-//Большая картинка
-function imgBigToggle() {
-  imgBig.classList.toggle("image-big_opened");
-}
-
-closeBigImg.addEventListener("click", imgBigToggle);
