884d82a439441ed3f248de3d2c9743cef6b65a0d
psevdo212 <104160364+psevdo212@users.noreply.github.com>
2022-10-26 20:22:52 +0300

    накосячил в классе

--- src/script/components/UserInfo.js
+++ src/script/components/UserInfo.js
@@ -1,23 +1,24 @@
 export default class UserInfo {
-  constructor({ username, subtitle, avatar }) {
-    this._username = document.querySelector(username);
-    this._subtitle = document.querySelector(subtitle);
-    this._avatar = document.querySelector(avatar);
+  constructor({ profileTitle, profileSubtitle, profileImg }) {
+    this._profileTitle = document.querySelector(profileTitle);
+    this._profileSubtitle = document.querySelector(profileSubtitle);
+    this._profileImg = document.querySelector(profileImg);
   }
 
   getUserInfo() {
     return {
-      name: this._username.textContent,
-      about: this._subtitle.textContent,
+      name: this._profileTitle.textContent,
+      about: this._profileSubtitle.textContent,
     };
   }
 
-  setUserInfo({ name, about, avatar }) {
-    this._username = name;
-    this._subtitle = about;
-    this._username.textContent = this._name;
-    this._subtitle.textContent = this._about;
+  setUserInfo ({ name, about, avatar, _id }) {
+    this._name = name;
+    this._about = about;
+    this._userId = _id;
+    this._profileTitle.textContent = this._name;
+    this._profileSubtitle.textContent = this._about;
     this._avatar = avatar;
-    this._avatar.src = this._avatar;
-  }
+    this._profileImg.src = this._avatar;
+  };
 }
